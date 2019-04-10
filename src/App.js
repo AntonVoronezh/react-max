718b6c7026a8263dee836824328e03375a34c778
AntonVoronezh <pstut@mail.ru>
2019-04-10 20:04:35 +0300

    add render

--- src/App.js
+++ src/App.js
@@ -31,7 +31,18 @@ class App extends React.Component {
     const nextNews = [data, ...this.state.news];
     this.setState({ news: nextNews });
   };
-  
+  render() {
+    const { news, isLoading } = this.state;
+
+    return (
+      <React.Fragment>
+        <Add onAddNews={this.handleAddNews} />
+        <h3>Новости</h3>
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
