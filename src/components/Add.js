6946e5d3f861df2c28621ec689052c76a09bbacd
AntonVoronezh <pstut@mail.ru>
2019-04-10 20:06:35 +0300

    add component

---
+++ src/components/Add.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+class Add extends React.Component {
+    state = {
+      name: "",
+      text: "",
+      bigText: "",
+      agree: false
+    };
+
+
+
+
+
+  }
+  
+
+
+export { Add };
