dd3e39a38dc4056a4b6eea4783ce01784782563d
AntonVoronezh <pstut@mail.ru>
2019-04-10 20:03:25 +0300

    add App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Add } from "./components/Add";
+import { News } from "./components/News";
+// import newsData from "./data/newsData";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    news: null,
+    isLoading: false
+  };
+
+
+
+
+  
+}
+
+export default App;
