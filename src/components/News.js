d33b607d9a26f66b572d8ae506dbcdf3f9c75937
AntonVoronezh <pstut@mail.ru>
2019-04-14 20:14:43 +0300

    8

---
+++ src/components/News.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Article } from "./Article";
+
+class News extends React.Component {
+  state = {
+    filteredNews: this.props.data
+  };
+
+
+  // componentWillReceiveProps(nextProps) {
+  //   // console.log({ nextProps })
+  //   // console.log({ oldProps: this.props })
+  //   let nextFilteredNews = [...nextProps.data];
+
+  //   nextFilteredNews.forEach((item, index) => {
+  //     if (item.bigText.toLowerCase().indexOf("pubg") !== -1) {
+  //       item.bigText = "СПАМ";
+  //     }
+  //   });
+
+  //   this.setState({ filteredNews: nextFilteredNews });
+  // }
+
+
+  render() {
+    const { filteredNews } = this.state;
+    return (
+
+    );
+  }
+}
+
