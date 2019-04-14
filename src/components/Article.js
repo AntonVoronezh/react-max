eec0801b98ad1adf7b69670928edc8a71ad46f7a
AntonVoronezh <pstut@mail.ru>
2019-04-14 20:12:17 +0300

    2

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,6 +9,7 @@ class Article extends React.Component {
         <div className="article">
           <p className="news__author">{author}:</p>
           <p className="news__text">{text}</p>
+          {visible && <p className="news__big-text">{bigText}</p>}
         </div>
       );
     }
